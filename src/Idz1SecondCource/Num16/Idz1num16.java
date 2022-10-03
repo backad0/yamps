4379a2c51f16ba1278916439fb7549812c319066
backad0 <91736145+backad0@users.noreply.github.com>
2022-10-03 10:03:05 +0600

    adding vector3d

--- src/Idz1SecondCource/Num16/Idz1num16.java
+++ src/Idz1SecondCource/Num16/Idz1num16.java
@@ -1,4 +1,23 @@
 package Idz1SecondCource.Num16;
 
+import Idz1SecondCource.Num15.Point3D;
+
+import static Idz1SecondCource.Num16.Vector3D.getEquality;
+
 public class Idz1num16 {
+    public static void main(String[] args) {
+        Point3D point1 = new Point3D(1, 1, 1);
+        Point3D point2 = new Point3D(2, 2, 2);
+
+        Vector3D p1p2 = new Vector3D(point1, point2);
+        Vector3D vector = new Vector3D(1, 1, 1);
+
+        boolean equality = getEquality(p1p2, vector);
+        if (!equality) {
+            System.out.println("Vectors are not equal");
+        } else {
+            System.out.println("Vectors are equal");
+
+        }
+    }
 }
---
+++ src/Idz1SecondCource/Num16/Vector3D.java
@@ -0,0 +1,52 @@
+package Idz1SecondCource.Num16;
+
+import Idz1SecondCource.Num15.Point3D;
+
+import static java.lang.Math.sqrt;
+
+public class Vector3D {
+    private double x, y, z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3D() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public Vector3D(Point3D a1, Point3D a2) {
+        x = a2.getX() - a1.getX();
+        y = a2.getY() - a1.getY();
+        z = a2.getZ() - a1.getZ();
+    }
+
+    public double getLength() {
+        double res;
+        res = sqrt(x * x + y * y + z * z);
+        return res;
+    }
+
+    public static boolean getEquality(Vector3D v1, Vector3D v2) {
+        boolean res = true;
+        double var1 = v1.getX() - v2.getX(), var2 = v1.getY() - v2.getY(), var3 = v1.getZ() - v2.getZ();
+        if ((var1 != var2) || (var1 != var3) || (var2 != var3)) res = false;
+        return res;
+    }
+}
